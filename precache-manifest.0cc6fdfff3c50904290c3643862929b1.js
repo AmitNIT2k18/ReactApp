62e2f4a655073c3581e11ccc2546d6ac88820957
Pratik Kalathiya <pratik.kalathiya@gmail.com>
2019-02-01 09:58:12 +0530

    Updates

---
+++ precache-manifest.0cc6fdfff3c50904290c3643862929b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ed0e35a7143e88756f4",
+    "url": "/ReactApp/static/css/main.3706e501.chunk.css"
+  },
+  {
+    "revision": "2ed0e35a7143e88756f4",
+    "url": "/ReactApp/static/js/main.2ed0e35a.chunk.js"
+  },
+  {
+    "revision": "ea947d66b816256e03f5",
+    "url": "/ReactApp/static/js/1.ea947d66.chunk.js"
+  },
+  {
+    "revision": "2e2b30ee4b57700ce9f2",
+    "url": "/ReactApp/static/js/runtime~main.2e2b30ee.js"
+  },
+  {
+    "revision": "f2785918d601dc7b4de799686f085043",
+    "url": "/ReactApp/index.html"
+  }
+];
